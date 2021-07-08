c75269b85a40bc9ff748d01a342475ec09cbf98c
Allen <allenalvin333@gmail.com>
2021-07-08 13:12:52 +0530

    Day 8

--- 8.py
+++ 8.py
@@ -4,7 +4,9 @@ d = {}
 for T in range(int(input())):
     a,b = map(str,input().split(" "))
     d[a]=b
-for T in range(len(d)):
-    query = input()
-    if query in d: print("{}={}".format(query,d[query]))
-    else: print("Not found")
+while True:
+    try:
+        query = input()
+        if query in d: print("{}={}".format(query,d[query]))
+        else: print("Not found")
+    except: break
