f02cc400fdb5a0c2054aece55fb7d5becf5a67ca
Allen <allenalvin333@gmail.com>
2021-07-20 08:23:52 +0530

    Day 19

---
+++ 19.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/30-interfaces/problem
+
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        l = []
+        for z in range(1,n//2+1): 
+            if(n%z==0): l.append(z)
+        return sum(l)+n
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
