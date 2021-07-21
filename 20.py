404ac7068a6b1d632b646a4645421c99ed3759a6
Allen <allenalvin333@gmail.com>
2021-07-21 09:15:41 +0530

    Day 20

--- 20.py
+++ 20.py
@@ -1,13 +1,5 @@
 # https://www.hackerrank.com/challenges/30-sorting/problem
 
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
 if __name__ == '__main__':
     n = int(input().strip())
     a = list(map(int, input().rstrip().split()))
