f0908962be1ef5496ce58a2ec488db864ff57f1f
allenalvin333 <allenalvin333@gmail.com>
2021-07-25 10:18:49 +0530

    Day 24

---
+++ 24.py
@@ -0,0 +1,40 @@
+# https://www.hackerrank.com/challenges/30-linked-list-deletion/problem
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+    def removeDuplicates(self,head):
+        if(head==None): return None
+        s=head
+        while(head.next != None):
+            if(head.data==head.next.data): head.next=head.next.next
+            else: head=head.next
+        return s
+    
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+head=mylist.removeDuplicates(head)
+mylist.display(head); 
