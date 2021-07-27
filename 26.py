8af2b402c873c9b07099bc70da2a8fc0fe85a940
allenalvin333 <allenalvin333@gmail.com>
2021-07-27 07:13:06 +0530

    Day 26

--- 26.py
+++ 26.py
@@ -3,7 +3,7 @@
 d2,m2,y2 = map(int,input().split())
 d1,m1,y1 = map(int,input().split())
 
-if (y2,m2,d2)<=(y1,m1,d1): print(0)
-elif (y2,m2)==(y1,m1): print((d2-d1)*15)
-elif (y2)==(y1): print((m2-m1)*500)
+if((y2,m2,d2)<=(y1,m1,d1)): print(0)
+elif((y2,m2)==(y1,m1)): print((d2-d1)*15)
+elif((y2)==(y1)): print((m2-m1)*500)
 else: print(10000)
